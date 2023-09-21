baf15061631bf5ef41bb8a489b8ba6680199221e
Andrii Ratsilevych <andrii.racilewicz@gmail.com>
2023-09-21 15:16:26 +0200

    add cors middleware

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import cors from "cors";
+// import cors from "cors";
+import corsMiddleware from "./middleware/cors.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
@@ -15,12 +16,15 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
-app.use(cors({ origin: ['*', 'https://moviemongo.vercel.app', 'https://vercel.com/feulife/moviemongo/a42a84aAbRoJsrcyXsNfCXdfhG6U', 'https://react-redux-movie-teal.vercel.app', 'https://vercel.com/feulife/react-redux-movie/FxmZzCSrPFyeqXc1yZ2d5YLcB5BR'] }));
+app.use(corsMiddleware)
 
+// app.use(cors());
+// app.use(cors({ origin: ['*', 'https://moviemongo.vercel.app', 'https://vercel.com/feulife/moviemongo/a42a84aAbRoJsrcyXsNfCXdfhG6U', 'https://react-redux-movie-teal.vercel.app', 'https://vercel.com/feulife/react-redux-movie/FxmZzCSrPFyeqXc1yZ2d5YLcB5BR'] }));
+
+// app.use("/api/movies", movieRoutes);
+// app.use("/api/users", userRoutes);
 app.use("/movies", movieRoutes);
 app.use("/users", userRoutes);
-
 const start = async () => {
   try {
     await mongoose.connect(Db, {
